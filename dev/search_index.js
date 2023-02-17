a0bacf5d7aa1747a6e6f69ffcf28a414175d3a47
Documenter.jl <documenter@juliadocs.github.io>
2023-02-17 18:31:14 +0000

    build based on 99980af

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Titus","category":"page"},{"location":"#Titus","page":"Home","title":"Titus","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Titus.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Titus]","category":"page"}]
+}
